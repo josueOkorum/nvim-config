5de9eff51bc77b789975ebbd8430a218de07d493
josueOkorum <josue.barreto@okorum.com>
2026-05-20 11:13:35 -0500

    add debugger (doesnt work)

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,3 +1,10 @@
+local js_based_languages = {
+  "typescript",
+  "javascript",
+  "typescriptreact",
+  "javascriptreact",
+  "vue",
+}
 return {
 	{
 		"mfussenegger/nvim-dap",
@@ -6,6 +13,49 @@ return {
 			"theHamsta/nvim-dap-virtual-text",
 			"nvim-neotest/nvim-nio",
 			"williamboman/mason.nvim",
+{
+        "microsoft/vscode-js-debug",
+        -- After install, build it and rename the dist directory to out
+        build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+        version = "1.*",
+      },
+ {
+        "mxsdev/nvim-dap-vscode-js",
+        config = function()
+          ---@diagnostic disable-next-line: missing-fields
+          require("dap-vscode-js").setup({
+            -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+            -- node_path = "node",
+
+            -- Path to vscode-js-debug installation.
+            debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+
+            -- Command to use to launch the debug server. Takes precedence over "node_path" and "debugger_path"
+            -- debugger_cmd = { "js-debug-adapter" },
+
+            -- which adapters to register in nvim-dap
+            adapters = {
+              "chrome",
+              "pwa-node",
+              "pwa-chrome",
+              "pwa-msedge",
+              "pwa-extensionHost",
+              "node-terminal",
+            },
+
+						port = '${port}',
+            -- Path for file logging
+            -- log_file_path = "(stdpath cache)/dap_vscode_js.log",
+
+            -- Logging level for output to file. Set to false to disable logging.
+            -- log_file_level = false,
+
+            -- Logging level for output to console. Set to false to disable console output.
+            -- log_console_level = vim.log.levels.ERROR,
+          })
+        end,
+      },
+
 		},
 		config = function()
 			local dap = require("dap")
@@ -14,31 +64,36 @@ return {
 			require("dapui").setup()
 			require("nvim-dap-virtual-text").setup({})
 
-			require("dap").adapters["pwa-node"] = {
-				type = "server",
-				host = "localhost",
-				port = 9229,
-				executable = {
-					command = "node",
-					-- 💀 Make sure to update this path to point to your installation
-					args = {
-						"/Users/josue.barreto/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-						9229,
-					},
-				},
-			}
+			-- require("dap").adapters["pwa-node"] = {
+			-- 	type = "server",
+			-- 	host = "localhost",
+			-- 	port = 9229,
+			-- 	executable = {
+			-- 		command = "node",
+			-- 		-- 💀 Make sure to update this path to point to your installation
+			-- 		args = {
+			-- 			"/Users/josue.barreto/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+			-- 			9229,
+			-- 		},
+			-- 	},
+			-- }
 
-			require("dap").configurations.javascript = {
-				{
-					type = "pwa-node",
-					request = "attach",
-					port = 9229,
-					name = "server",
-					address = "127.0.0.1",
-					localRoot = "${workspaceFolder}",
-					skipFiles = { "<node_internals>/**" },
-				},
+      for _, language in ipairs(js_based_languages) do
+
+			require("dap").configurations[language] = {
+{
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+port = 9229,
+          }
+				
 			}
+			end
+
 
 			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
